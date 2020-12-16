2eabd80e3a7b3340125834ef976daaa7aad4b2f2
alex ghose <alex.ghose@generalassemb.ly>
2020-12-16 09:55:48 -0500

    add detail page - user can lookup albums using id

--- routes/albums.js
+++ routes/albums.js
@@ -6,9 +6,22 @@ let albumsArray = [
     {id:"a1", name: "Baby One More Time", artist:"Britney"},
 ]
 
+function getName(incoming_id) {
+    for (let current_album of albumsArray) {
+        if (current_album.id == incoming_id) {
+            return current_album.name
+        }
+    }
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('albums.ejs', {albumsArray});
 });
+
+router.get('/:id', function(req,res) {
+    let myname = getName(req.params.id)
+    res.send("you have typed " + req.params.id + " which is album name " + myname)
+})
 
 module.exports = router;
