d2353e3b943a9b2f54fcef164c0ee19bb47da6cf
alex ghose <alex.ghose@generalassemb.ly>
2020-12-16 09:19:46 -0500

    add database and render it using embedded javascript

--- routes/albums.js
+++ routes/albums.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let albumsArray = [
+    {id:"a0", name: "Thriller", artist: "MJ"},
+    {id:"a1", name: "Baby One More Time", artist:"Britney"},
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('albums.ejs');
+  res.render('albums.ejs', {albumsArray});
 });
 
 module.exports = router;
